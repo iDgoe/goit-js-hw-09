5157e8c18ea4abc9388c2ccc2a83e654162d4976
iDgoe <e.novykov@gmail.com>
2023-04-03 19:13:07 +0300

    dz1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startButton = document.querySelector('[data-start]');
+console.log(startButton);
+const stopButton = document.querySelector('[data-stop]');
+console.log(stopButton);
+const body = document.querySelector('body');
+console.log(body);
 
+let set = null;
+
+startButton.addEventListener('click', onStart);
+stopButton.addEventListener('click', onStop);
+
+function onStart() {
+  set = setInterval(getBgColor, 1000);
+  startButton.toggleAttribute('disabled');
+}
+
+function onStop() {
+  clearInterval(set);
+  startButton.removeAttribute('disabled');
+}
+
+function getBgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
